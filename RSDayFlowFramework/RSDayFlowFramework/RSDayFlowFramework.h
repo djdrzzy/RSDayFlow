58b097711285423b2e5cb144ad91b25fffa7366d
Daniel Drzimotta <djdrzzy@gmail.com>
2015-09-01 19:59:16 -0600

    Added a project to get carthage support.

---
+++ RSDayFlowFramework/RSDayFlowFramework/RSDayFlowFramework.h
@@ -0,0 +1,19 @@
+//
+//  RSDayFlowFramework.h
+//  RSDayFlowFramework
+//
+//  Created by Daniel Drzimotta on 2015-09-01.
+//  Copyright (c) 2015 Daniel Drzimotta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSDayFlowFramework.
+FOUNDATION_EXPORT double RSDayFlowFrameworkVersionNumber;
+
+//! Project version string for RSDayFlowFramework.
+FOUNDATION_EXPORT const unsigned char RSDayFlowFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSDayFlowFramework/PublicHeader.h>
+
+
